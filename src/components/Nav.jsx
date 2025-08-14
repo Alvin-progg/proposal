cece999582fa8ebb5322a1d47b858beba71d895f
ALvin-progg <doctoralvim726@gmail.com>
2025-08-14 15:10:13 +0800

    setup

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,19 +8,19 @@ const Nav = () => {
     <nav className='flex justify-between m-10 items-center'>
         <a href="#" className='font-bold'>BLUEKET</a>
         <ul className='flex gap-7'>
-            <li className='flex gap-1 items-center'>Demo <span><RiArrowDownWideFill /></span></li>
-            <li className='flex  gap-1  items-center'>About us <span><RiArrowDownWideFill /></span></li>
-            <li className='flex   gap-1 items-center'>Services <span><RiArrowDownWideFill /></span></li>
-            <li className='flex gap-1  items-center'>Portfolio <span><RiArrowDownWideFill /></span></li>
-            <li className='flex gap-1  items-center'>Blogs <span><RiArrowDownWideFill /></span></li>
-            <li className='flex gap-1  items-center'>Contact Us <span><RiArrowDownWideFill /></span></li>
+            <li className=''><a href="#" className='flex gap-1 items-center'>Demo <span><RiArrowDownWideFill /></span></a></li>
+            <li className=''><a href="#" className='flex gap-1 items-center'>About us <span><RiArrowDownWideFill /></span></a></li>
+            <li className=''><a href="#" className='flex gap-1 items-center'>Services <span><RiArrowDownWideFill /></span></a></li>
+            <li className=''><a href="#" className='flex gap-1 items-center'>Portfolio <span><RiArrowDownWideFill /></span></a></li>
+            <li className=''><a href="#" className='flex gap-1 items-center'>Blogs <span><RiArrowDownWideFill /></span></a></li>
+            <li className=''><a href="#" className='flex gap-1 items-center'>Contact Us <span><RiArrowDownWideFill /></span></a></li>
         </ul>
         <div className='flex items-center  gap-10'>
-            <button><FaPhone /></button>
+            <button className='bg-blue-500 text-white p-3 rounded-full'><FaPhone /></button>
             <a href="#" className='flex gap-2 items-center'>Get Started <span><CgArrowTopRight /></span></a>
         </div>
 
-
+        
     </nav>
   )
 }
