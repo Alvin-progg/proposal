ccd0de7e7ce3f10d2de516802173bf812a57d950
ALvin-progg <doctoralvim726@gmail.com>
2025-08-15 20:23:54 +0800

    eq12d931

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -9,9 +9,11 @@ const Services = () => {
         
 
         <div className='mx-auto w-[50vw] items-center justify-center mt-20'>
-            <div className='p-5 flex-col items-center justify-center gap-10'>
-                <h2 className='text-5xl '>Service We Provide</h2>
-                <p>Blueket overcomes challenges, achieves results, and adds value to our clients and partners. Take a look at some of our clients' success stories.</p>
+            <div className='p-5 flex-col items-center justify-center gap-10 px-3'>
+                <h2 className='text-4xl justify-center-safe flex mb-5 font-bold '>Service We Provide</h2>
+                <h3 className='justify-center-safe flex'>Blueket overcomes challenges, achieves results, and adds value to our clients and partners.</h3>
+                <h3 className='justify-center-safe flex'>Take a look at some of our clients' success stories.</h3>
+                
             </div>
             
         </div>
