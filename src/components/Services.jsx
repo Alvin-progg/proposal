67cdbd17263a3f08d1af76f2e4887e0b3742140b
ALvin-progg <doctoralvim726@gmail.com>
2025-08-17 19:27:20 +0800

    31djqdq

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -15,7 +15,7 @@ const Services = () => {
         <div className='  items-center justify-center mt-20 '>
             <div className='p-5 flex-col items-center justify-center text-wrap gap-10 px-3 lg:w-[60vw] sm:mx-auto '>
                 <h2 className=' justify-center-safe flex mb-5 font-bold text-2xl py-4 sm:text-4xl '>Service We Provide</h2>
-                <h3 className='justify-center-safe flex font-light  text-center  '>Blueket overcomes challenges, achieves results, and adds value to our clients and partners.Take a look at some of our clients' success stories.</h3>
+                <h3 className='justify-center-safe flex font-light  text-center  mx-auto w-[375px] sm:w-[540px] lg:w-[641px]  '>Blueket overcomes challenges, achieves results, and adds value to our clients and partners.Take a look at some of our clients' success stories.</h3>
             </div>
             
         </div>
