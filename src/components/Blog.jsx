a8c533784a84583bfa7153d8e5bd5002ac59d6a8
ALvin-progg <doctoralvin726@gmail.com>
2025-08-24 19:18:29 +0800

    2d22

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -12,41 +12,19 @@ const Blog = () => {
         <p>Stay Updated</p>
         <h2>News & blog Updates</h2>
       </div>
-      <div className="h-[582px] w-[431px] flex items-center justify-center mx-auto p-4 ">
-        <div className="flex flex-col items-center p-5">
-          <div className="flex justify-between items-center gap-15 my-5 ">
+      <div className=" flex items-center justify-center mx-auto p-4 w-[1296px] border-1 rounded-2xl border-border-200 ">
+        <div className="h-[582px] w-[431px] flex-col flex justify-between px-5 border-r border-border-200   ">
+          <div className="flex justify-between items-center mt-5 ">
             <h3>March 7, 2022</h3>
             <div className="flex items-center justify-center gap-1.5">
               <p>110</p>
               <FaEye />
             </div>
           </div>
-          <img src={UI} alt="UI design img" loading="lazy" />
+          <img src={UI} alt="UI design img" loading="lazy" className="  w-[380px] h-[250px]" />
           <h3>Free UI/UX Design Tutorial & Tools: The Learn UI Design Blog</h3>
           <p>
-            A Design Blog by Tom Smile on Typography,Color,Layout,UX,Design
-            Process
-          </p>
-          <div className="flex justify-between items-center mx-auto">
-            <p>Read Article</p>
-            <a href="#">
-              <FaLongArrowAltRight />
-            </a>
-          </div>
-        </div>
-        <div>
-          <div className="flex justify-between ">
-            <h3>March 7, 2022</h3>
-            <div className="flex items-center justify-center">
-              <p>110</p>
-              <FaEye />
-            </div>
-          </div>
-          <img src={Marketing} alt="UI design img" loading="lazy" />
-          <h3>Free UI/UX Design Tutorial & Tools: The Learn UI Design Blog</h3>
-          <p>
-            A Design Blog by Tom Smile on Typography,Color,Layout,UX,Design
-            Process
+            A design blog by Tom Smile on typography, color, layout, UX, design process.
           </p>
           <div className="flex justify-between">
             <p>Read Article</p>
@@ -55,19 +33,38 @@ const Blog = () => {
             </a>
           </div>
         </div>
-        <div>
-          <div className="flex justify-between ">
+        <div className="h-[582px] w-[431px] flex-col flex justify-between px-5 border-r border-border-200  ">
+          <div className="flex justify-between mt-5 ">
             <h3>March 7, 2022</h3>
-            <div className="flex items-center justify-center">
+            <div className="flex items-center justify-center gap-1.5">
               <p>110</p>
               <FaEye />
             </div>
           </div>
-          <img src={App} alt="UI design img" loading="lazy" />
+          <img src={UI} alt="UI design img" loading="lazy" className="  w-[380px] h-[250px]" />
           <h3>Free UI/UX Design Tutorial & Tools: The Learn UI Design Blog</h3>
           <p>
-            A Design Blog by Tom Smile on Typography,Color,Layout,UX,Design
-            Process
+            A design blog by Tom Smile on typography, color, layout, UX, design process.
+          </p>
+          <div className="flex justify-between">
+            <p>Read Article</p>
+            <a href="#">
+              <FaLongArrowAltRight />
+            </a>
+          </div>
+        </div>
+        <div className="h-[582px] w-[431px] flex-col flex justify-between px-5   ">
+          <div className="flex justify-between mt-5 ">
+            <h3>March 7, 2022</h3>
+            <div className="flex items-center justify-center gap-1.5">
+              <p>110</p>
+              <FaEye />
+            </div>
+          </div>
+          <img src={UI} alt="UI design img" loading="lazy" className="  w-[380px] h-[250px]" />
+          <h3>Free UI/UX Design Tutorial & Tools: The Learn UI Design Blog</h3>
+          <p>
+            A design blog by Tom Smile on typography, color, layout, UX, design process.
           </p>
           <div className="flex justify-between">
             <p>Read Article</p>
